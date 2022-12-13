d58f92b997a334a6d78c8d7708db1526ea44c30b
Shayeri Das <45433618+Shayeri15@users.noreply.github.com>
2022-12-13 16:34:06 +0530

    Add files for Hello world spring app

---
+++ awsscenarios/src/main/java/com/awstest/examples/awsscenarios/AwsscenariosApplication.java
@@ -0,0 +1,16 @@
+package com.awstest.examples.awsscenarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class AwsscenariosApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(AwsscenariosApplication.class, args);
+	}
+	
+
+}
---
+++ awsscenarios/src/test/java/com/awstest/examples/awsscenarios/AwsscenariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awstest.examples.awsscenarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsscenariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
